13669b6c5d70e082567a6d276932a49d5e8411c3
Alex Fourmy <alexfourmy@gmail.com>
2021-04-25 10:46:16 +0200

    added get post update delete functions

--- script.js
+++ script.js
@@ -2,6 +2,54 @@ const http = new easyHTTP
 
 // Get posts
 
+// http.get('https://jsonplaceholder.typicode.com/posts',
+// function(err, posts) {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(posts)
+//   }
+// })
+
+
+// Get single post
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1',
+// function(err, posts) {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(posts)
+//   }
+// })
+
+// Create Data
+
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//         console.log(err)
+//       } else {
+//         console.log(post)
+//       }
+// })
+
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+  if(err) {
+            console.log(err)
+          } else {
+            console.log(post)
+          }
+})
+
+// Delete post
+
 http.get('https://jsonplaceholder.typicode.com/posts',
 function(err, posts) {
   if(err) {
